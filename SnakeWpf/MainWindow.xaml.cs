3fc7b84df0578aef677bb9250e18c560828a4a44
harckline <antoinecronier1@gmail.com>
2019-11-27 11:32:58 +0100

    move one tile in grid

--- SnakeWpf/MainWindow.xaml.cs
+++ SnakeWpf/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SnakeWpf.UserControls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,56 @@ namespace SnakeWpf
     /// </summary>
     public partial class MainWindow : Window
     {
+        private SnakeTileUC tile;
+
         public MainWindow()
         {
             InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+            EventManager.RegisterClassHandler(typeof(Window),
+            Keyboard.KeyDownEvent, new KeyEventHandler(KeyDown), true);
+        }
+
+        private void KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Up:
+                    tile.MoveUp();
+                    break;
+                case Key.Right:
+                    tile.MoveRight();
+                    break;
+                case Key.Left:
+                    tile.MoveLeft();
+                    break;
+                case Key.Down:
+                    tile.MoveDown();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.MainGrid.Background = new SolidColorBrush(Color.FromArgb(50, 100, 150, 100));
+            for (int i = 0; i < 16; i++)
+            {
+                this.MainGrid.RowDefinitions.Add(new RowDefinition());
+            }
+
+            for (int i = 0; i < 16; i++)
+            {
+                this.MainGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+
+            tile = new SnakeTileUC(this.MainGrid);
+            tile.MoveTo(this.MainGrid.RowDefinitions.Count / 2, this.MainGrid.ColumnDefinitions.Count / 2);
+            
+            this.MainGrid.Children.Add(tile);
         }
     }
 }
---
+++ SnakeWpf/UserControls/SnakeTileUC.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SnakeWpf.UserControls
+{
+    /// <summary>
+    /// Logique d'interaction pour SnakeUC.xaml
+    /// </summary>
+    public partial class SnakeTileUC : UserControl
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+        public Grid Container { get; set; }
+
+        public SnakeTileUC()
+        {
+            InitializeComponent();
+            this.tile.Stroke = new SolidColorBrush(Color.FromRgb(0, 111, 0));
+            this.tile.Fill = new SolidColorBrush(Color.FromRgb(0, 111, 111));
+        }
+
+        public SnakeTileUC(Grid container) : this()
+        {
+            this.Container = container;
+        }
+
+        public void MoveTo(int row, int column)
+        {
+            this.Row = row;
+            this.Column = column;
+
+            Grid.SetRow(this, row);
+            Grid.SetColumn(this, column);
+        }
+
+        public void MoveUp()
+        {
+            if (this.Row - 1 >= 0)
+            {
+                this.MoveTo(this.Row - 1, this.Column);
+            }
+        }
+
+        public void MoveRight()
+        {
+            if (this.Column + 1 < this.Container.ColumnDefinitions.Count)
+            {
+                this.MoveTo(this.Row, this.Column + 1);
+            }
+        }
+
+        public void MoveLeft()
+        {
+            if (this.Column - 1 >= 0)
+            {
+                this.MoveTo(this.Row, this.Column - 1);
+            }
+        }
+
+        public void MoveDown()
+        {
+            if (this.Row + 1 < this.Container.RowDefinitions.Count)
+            {
+                this.MoveTo(this.Row + 1, this.Column);
+            }
+        }
+    }
+}
